2d8fa8ba15d0b930ba49bbec4b5af29b7031a74d
Davi Esteves <davi.esteves@estudante.ufjf.br>
2023-04-10 15:20:04 -0300

    x_test variable bug fixed

--- Models/SubModules.py
+++ Models/SubModules.py
@@ -2,6 +2,7 @@ import math
 
 import torch
 from torch import nn
+import torch.nn.functional as F
 
 
 class DepthWiseConv2d(nn.Module):
@@ -95,4 +96,108 @@ class Unsqueeze(nn.Module):
         super().__init__()
 
     def forward(self, x):
-        return x.view((x.shape[1], x.shape[0], 1))
+        return x.view((x.shape[0], x.shape[1], 1))
+
+
+class EmbeddingLayer(nn.Module):
+    def __init__(
+            self,
+            n_times,
+            patches_num,
+            embedding_dim,
+            dropout_rate,
+            depth=1
+    ):
+        super().__init__()
+        self.patches_num = patches_num
+        self.embedding = nn.Linear(in_features=math.ceil(n_times * depth / self.patches_num),
+                                   out_features=embedding_dim)
+        self.dropout = nn.Dropout(dropout_rate)
+        self.positional_encode_table = nn.Embedding(num_embeddings=self.patches_num, embedding_dim=embedding_dim)
+
+    # output shape -> [Batches, embeding dim, pathces,]
+    def forward(self, x):
+        patches = list(x.chunk(self.patches_num, dim=-1))
+        patches[-1] = F.pad(patches[-1], (0, patches[0].shape[-1] - patches[-1].shape[-1]))
+        patches = torch.stack(patches, dim=-2)
+
+        # creating new dimesion for embedding (its not -1 to not bug the matrix mul)
+        patches = patches.unsqueeze(-2)
+        embedding_result = self.embedding(patches)
+
+        # creating removing the unnecessary dimension to add the positional encode
+        embedding_result = embedding_result.squeeze(-2)
+        positional_result = embedding_result + self.positional_encode_table(
+            (torch.arange(self.patches_num)))
+        out = self.dropout(positional_result)
+
+        return out.view(out.shape[0], out.shape[2], out.shape[1])
+        # return out.view(out.shape[0], out.shape[1], out.shape[3], out.shape[2])
+
+
+class VisionTransformer(nn.Module):
+    def __init__(
+            self,
+            patches,
+            transformer_ffd,
+            nhead=8,
+            num_layers=2
+    ):
+        super().__init__()
+        self.transformer = nn.Sequential(
+            nn.TransformerEncoder(
+                nn.TransformerEncoderLayer(d_model=patches, dim_feedforward=transformer_ffd, nhead=nhead,
+                                           batch_first=True),
+                num_layers=num_layers,
+            ),
+        )
+
+    def forward(self, x):
+        return self.transformer(x)
+
+
+class ConvolutionSimplifier(nn.Module):
+    def __init__(self, embedding_dim, dropout_rate):
+        super().__init__()
+        self.conv = nn.Sequential(
+            nn.AdaptiveAvgPool1d(output_size=1),
+            nn.Dropout(dropout_rate),
+            nn.Flatten(),
+            nn.LayerNorm(normalized_shape=embedding_dim),
+            nn.ELU(),
+        )
+
+    def forward(self, x):
+        return self.conv(x)
+
+
+class FeatureExtraction(nn.Module):
+    def __init__(
+            self,
+            n_channels,
+            kernel_length,
+            F1,
+            D,
+            F2,
+            pool1_stride,
+            pool2_stride,
+    ):
+        super().__init__()
+        self.net = nn.Sequential(
+            ViewConv(),
+            nn.Conv2d(in_channels=n_channels, out_channels=F1, kernel_size=(1, kernel_length), bias=False,
+                      padding='same'),
+            nn.BatchNorm2d(num_features=F1, momentum=0.01, eps=0.001, track_running_stats=False),
+            DepthWiseConv2d(in_channels=F1, kernel_size=(n_channels, 1), kernels_per_layer=D, bias=False),
+            nn.BatchNorm2d(num_features=F1 * D, momentum=0.01, eps=0.001, track_running_stats=False),
+            nn.ELU(),
+            nn.AvgPool2d(kernel_size=(1, pool1_stride), stride=pool1_stride),
+            SeparableConv2d(in_channels=F1 * D, kernel_size=(1, 16), out_channels=F2, bias=False),
+            nn.BatchNorm2d(num_features=F2, momentum=0.01, eps=0.001, track_running_stats=False),
+            nn.ELU(),
+            nn.AvgPool2d(kernel_size=(1, pool2_stride), stride=pool2_stride),
+            nn.Flatten(),
+        )
+
+    def forward(self, x):
+        return self.net(x)
--- modules/TrainTester.py
+++ modules/TrainTester.py
@@ -4,54 +4,92 @@ from sklearn.metrics import cohen_kappa_score
 from sklearn.metrics import auc
 
 
+import torch
+from matplotlib import pyplot as plt
+from sklearn.metrics import cohen_kappa_score
+from sklearn.metrics import auc
+
+
 class TrainerTester:
 
     @staticmethod
-    def train_loop(model, optimizer, X, y, lr, ud, batch_size=32, iterations=1000):
+    def train_loop(model, optimizer, X_train, y_train, X_test, y_test, lr, ud, batch_size=32, iterations=1000):
         lossi = []
+        accuracyi = []
+        lossv = []
+        accuracyv = []
         for k in range(iterations):
-            batch_indexes = torch.randint(0, X.shape[0], (batch_size,))
-            X_batch, y_batch = X[batch_indexes], y[batch_indexes] # batch X,Y
-            pred, loss, out_values = model(X_batch, y_batch)
+            batch_indexes = torch.randint(0, X_train.shape[0], (batch_size,))
+            batch_test_indexes = torch.randint(0, X_test.shape[0], (batch_size,))
+
+            X_batch, y_batch = X_train[batch_indexes], y_train[batch_indexes]  # train batch X,Y
+
+            #data augmentation
+            # X_batch, _ = transforms['freq'].operation(X_batch, None, 10., sfreq)
+            # X_batch, _ = transforms['gauss'].operation(X_batch, None, std=1e-5)
+
+            X_test_batch, y_test_batch = X_test[batch_test_indexes], y_test[batch_test_indexes]  # test batch X,Y
+
+            pred, loss, out_values = model(X_batch,  y_batch)
 
             # Backpropagation
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
 
-            #tracking
-            lossi.append(loss.item())
-
             with torch.no_grad():
-                if k % 100 == 99:
-                    print(f"loss: {loss} iteration: {k}/{iterations}")
-                    plt.plot(torch.tensor(lossi).view(-1, 10).mean(dim=1).log10().tolist())
+                #tracking training metrics
+                lossi.append(loss.item())
+                accuracyi.append((pred.argmax(1) == y_batch).type(torch.float32).sum().item() / y_batch.shape[0])
+
+                #tracking test metrics
+                test_loss, test_accuracy = TrainerTester.test_loss(model, X_test_batch, y_test_batch)
+                lossv.append(test_loss)
+                accuracyv.append(test_accuracy)
+
+                if (k+1) % 100 == 0:
+                    print(f"loss: {loss} iteration: {k+1}/{iterations}")
+                    plt.title('loss')
+                    plt.plot(torch.tensor(lossv).view(-1, 10).mean(dim=1).tolist(), label="test loss")
+                    plt.plot(torch.tensor(lossi).view(-1, 10).mean(dim=1).tolist(), label="train loss")
                     plt.show()
 
-                ud.append([((lr*p.grad).std() / p.data.std()).log10().item() for p in model.parameters()])
+                    plt.title('accuracy')
+                    plt.plot(torch.tensor(accuracyv).view(-1, 10).mean(dim=1).tolist(), label="test accuracy")
+                    plt.plot(torch.tensor(accuracyi).view(-1, 10).mean(dim=1).tolist(), label="train accuracy")
+                    plt.show()
+
+                ud.append([((lr * p.grad).std() / p.data.std()).item() for p in model.parameters()])
 
         return lossi
 
     @staticmethod
-    def test_loop(model, Xtest, ytest):
+    def test_loss(model, X_test,  y_test):
         model.eval()
-        X_batch, y_batch = Xtest, ytest
-
         with torch.no_grad():
-            pred, loss, out_values = model(X_batch, y_batch)
-            correct = (pred.argmax(1) == y_batch).type(torch.float32).sum().item() / y_batch.shape[0]
-            kappa = cohen_kappa_score(pred.argmax(1), y_batch)
+            pred, loss, out_values = model(X_test, y_test)
+            accuracy = (pred.argmax(1) == y_test).type(torch.float32).sum().item() / y_test.shape[0]
+        model.train()
+        return loss, accuracy
 
-        print(f"Test loss: {loss:>8f} \n Accuracy: {correct:>8f} \n kappa: {kappa} \n")
+    @staticmethod
+    def test_loop(model, X_test,  y_test):
+        model.eval()
+        with torch.no_grad():
+            pred, loss, out_values = model(X_test, y_test)
+            accuracy = (pred.argmax(1) == y_test).type(torch.float32).sum().item() / y_test.shape[0]
+            kappa = cohen_kappa_score(pred.argmax(1), y_test)
+
+        print(f"Test loss: {loss:>8f} \n Accuracy: {accuracy:>8f} \n kappa: {kappa} \n")
         model.train()
         return out_values
 
     @staticmethod
-    def test_and_show(model, Xtest, ytest):
+    def test_and_show(model, X_test,  y_test):
         model.eval()
-        X_batch, y_batch = Xtest, ytest
         with torch.no_grad():
-            pred, loss, out_values = model(X_batch, y_batch)
-            accuracy = pred.argmax(1) == y_batch
+            pred, loss, out_values = model(X_test,  y_test)
+            accuracy = pred.argmax(1) == y_test
             model.train()
             return accuracy
+
